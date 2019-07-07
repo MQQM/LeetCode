7df471d03301925e4eb6d58a75e42cf388e299ae
Tianhao Zhang <tianhao.hdu@gmail.com>
2019-07-07 22:36:52 +0800

    Create MQQM.cpp

---
+++ 0350_Intersection_of_Two_Arrays_II/MQQM.cpp
@@ -0,0 +1,30 @@
+/*
+  题目：
+  给定两个无序数组，编写一个函数来计算它们的交集。
+
+  示例 1:
+  输入: nums1 = [1,2,2,1], nums2 = [2,2]
+  输出: [2,2]
+  示例 2:
+  输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+  输出: [4,9]
+*/
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> m;
+        for(int i=0; i<nums1.size(); i++){
+            m[nums1[i]]++;
+        }
+        
+        vector<int> rst;
+        for(int i=0; i<nums2.size(); i++){
+            if(m[nums2[i]] > 0){
+                rst.push_back(nums2[i]);
+                m[nums2[i]]--;
+            }
+        }        
+        
+        return rst;
+    }
+};
