455f126ad32acbf78c3bf6cf1d7224e5f693a5fc
Tianhao Zhang <tianhao.hdu@gmail.com>
2019-08-28 09:58:23 +0800

    Create MQQM.cpp

---
+++ 0434_Number_of_Segments_in_a_String/MQQM.cpp
@@ -0,0 +1,21 @@
+/*
+  题目：
+  统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+  请注意，你可以假定字符串里不包括任何不可打印的字符。
+
+  示例:
+  输入: "Hello, my name is John"
+  输出: 5
+*/
+class Solution {
+public:
+    int countSegments(string s) {
+        stringstream ss(s);
+        string word;
+        int cnt=0;
+        while(ss >> word){
+            cnt++;
+        }
+        return cnt;
+    }
+};
