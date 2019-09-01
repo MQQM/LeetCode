3433b4f492a6e92b367b5b9dbaf4e845728d3bda
Tianhao Zhang <tianhao.hdu@gmail.com>
2019-09-01 13:06:26 +0800

    Create MQQM.cpp

---
+++ 0031_Next_Permutation/MQQM.cpp
@@ -0,0 +1,36 @@
+/*
+  题目：
+  实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+  如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+  必须原地修改，只允许使用额外常数空间。
+
+  以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+  1,2,3 → 1,3,2
+  3,2,1 → 1,2,3
+  1,1,5 → 1,5,1
+
+  https://leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-by-leetcode/
+*/
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int j = nums.size() - 2;    
+        for (; j >= 0; j--){          
+            if (nums[j] < nums[j + 1]){                
+                break;       
+            }  
+        }
+        
+        if (j >= 0){          
+            int k = nums.size() - 1;
+            for (; k > j; k--){                            
+                if (nums[k] > nums[j]){                     
+                    break;           
+                }       
+            }      
+            swap(nums[j], nums[k]);
+        }
+        
+        sort(nums.begin() + j + 1, nums.end());
+    }
+};
