18881071bfcb06eb638f85394dcbbda04a19330a
Tianhao Zhang <tianhao.hdu@gmail.com>
2019-09-10 13:33:48 +0800

    Update MQQM.cpp

--- 0125_Valid_Palindrome/MQQM.cpp
+++ 0125_Valid_Palindrome/MQQM.cpp
@@ -1,31 +1,44 @@
 /*
   题目：
-  给定一个字符串，验证它是否是回文串(前后对称)。只考虑字母和数字。
+  给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+  说明：本题中，我们将空字符串定义为有效的回文串。
+
+  示例 1:
+  输入: "A man, a plan, a canal: Panama"
+  输出: true
+
+  示例 2:
+  输入: "race a car"
+  输出: false  
 */
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
+    bool isPalindrome(string s)
+    {
         int left = 0;
         int right = s.size() - 1;
-       while (left < right){
-           if( !isAlphaNum(s[left]) ){
-               ++left;
-           }else if(!isAlphaNum(s[right])) {
-               --right;
-           }else if ( (s[left] + 32 - 'a') % 32 != (s[right] + 32 - 'a') % 32 ){
-               return false;
-           }else {				
-               ++left; --right;			
-           }
-		    }
-    
+        while (left < right)
+        {
+            if (!isalpha(s[left]) && !isdigit(s[left]))
+            {
+                left++;
+            }
+            else if (!isalpha(s[right]) && !isdigit(s[right]))
+            {
+                right--;
+            }
+            else if ((s[left] + 32 - 'a') % 32 != (s[right] + 32 - 'a') % 32)
+            {
+                return false;
+            }
+            else
+            {
+                left++;
+                right--;
+            }
+        }
         return true;
-    }
-    bool isAlphaNum(char ch){
-        if(ch >= 'a' && ch <= 'z')  return true;
-        if(ch >= 'A' && ch <= 'Z')  return true;
-        if(ch >= '0' && ch <= '9')  return true;
-        
-        return false;
     }
 };
