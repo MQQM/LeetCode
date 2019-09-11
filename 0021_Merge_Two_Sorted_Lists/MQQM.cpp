0a1f5dcceb9230e845557728cc984e7adcf20989
Tianhao Zhang <tianhao.hdu@gmail.com>
2019-09-11 14:41:00 +0800

    Update MQQM.cpp

--- 0021_Merge_Two_Sorted_Lists/MQQM.cpp
+++ 0021_Merge_Two_Sorted_Lists/MQQM.cpp
@@ -1,7 +1,11 @@
 /*
   题目：
-  将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
-  
+  将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+  示例：
+  输入：1->2->4, 1->3->4
+  输出：1->1->2->3->4->4
+
   做法：递归
   
   参考：
@@ -16,29 +20,35 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL && l2 == NULL){
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        if (l1 == NULL && l2 == NULL)
+        {
             return NULL;
-        }else{           
-            if(l1 == NULL){            
-                return l2;        
-            }       
-            if(l2 == NULL){           
-                return l1;       
-            }
-        }        
-        
-        ListNode* head = NULL;
-        if(l1->val > l2->val){
-            head = l2;
-            head->next = mergeTwoLists(l1, l2->next);
-        }else{
+        }
+        if (l1 == NULL)
+        {
+            return l2;
+        }
+        if (l2 == NULL)
+        {
+            return l1;
+        }
+
+        ListNode *head = NULL;
+        if (l1->val < l2->val)
+        {
             head = l1;
             head->next = mergeTwoLists(l1->next, l2);
-        }       
- 
+        }
+        else
+        {
+            head = l2;
+            head->next = mergeTwoLists(l1, l2->next);
+        }
         return head;
     }
 };
