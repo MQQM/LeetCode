d75e2083b72720c2be08e902fd3dbfd0422b5579
Tianhao Zhang <tianhao.hdu@gmail.com>
2020-01-22 11:52:30 +0800

    Create MQQM.go

---
+++ 0021_Merge_Two_Sorted_Lists/MQQM.go
@@ -0,0 +1,43 @@
+/*
+  题目：
+  将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+  
+  示例：
+  输入：1->2->4, 1->3->4
+  输出：1->1->2->3->4->4
+  
+  做法：递归
+  
+  参考：
+  https://www.cnblogs.com/leavescy/p/5879625.html
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	var head *ListNode = nil
+	if l1.Val < l2.Val {
+		head = l1
+		head.Next = mergeTwoLists(l1.Next, l2)
+	} else {
+		head = l2
+		head.Next = mergeTwoLists(l1, l2.Next)
+	}
+
+	return head
+}
